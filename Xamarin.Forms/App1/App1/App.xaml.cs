9db2231f87ddc040f4a6328b650689596a9a8272
Carlos Graniel <caco572@hotmail.com>
2018-11-25 22:52:51 -0600

    first commit

---
+++ Xamarin.Forms/App1/App1/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace App1
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            //InitializeComponent();
+
+            //MainPage = new MainPage();
+            MainPage = new ContentPage()
+            {
+                Content = new StackLayout
+                {
+                    VerticalOptions = LayoutOptions.Center,
+                    Children =
+                    {
+                        new Label
+                        {
+                            HorizontalTextAlignment = TextAlignment.Center,
+                            Text = "Welcome to Xamarin Forms"
+                        }
+                    }
+                }
+            };
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
